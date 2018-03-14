6bfd75e719258453a2a67c5ca20dd12399441348
Eric McKinney <mckinney99@gmail.com>
2018-03-14 19:39:24 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,34 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :update]
 
+  def index
+    games = Game.all
+    render json: games
+  end
+
+  def show
+    render json: @game
+  end
+
+  def create
+    game = Game.create(game_params)
+    render json: game, status: 201
+  end
+
+  def update
+    @game.update(game_params)
+    render json: @game
+  end
+
+  private
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.permit(state: [])
+  end
+
+end
+
 end
